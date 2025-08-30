3ff690e6c0456e0e0e6c4af82323b0a92a780f2c
Abhinav Bombale <abhinav.bombale@gmail.com>
2025-08-30 21:45:28 +0530

    Working Face detection based on facial data xml files

--- main.py
+++ main.py
@@ -7,7 +7,22 @@ face_cap = cv2.CascadeClassifier("C:/Users/Abhinav/appdata/local/programs/python
 video_cap = cv2.VideoCapture(0)
 while True:
     ret , video_data = video_cap.read()
-    col = cv2.cvtColor(video_data, )
+
+    col = cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
+    faces = face_cap.detectMultiScale(
+        col,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(video_data, (x,y), (x+w,y+h), (0,0,0,),2)
+
+
+
+
     cv2.imshow("video_live",video_data)
     if cv2.waitKey(10) == ord("a"):
         break   
